bae333049a89725bad305e212470e742c5916149
임준영 <a79007714@gmail.com>
2022-07-23 00:35:08 +0900

    chore: Inflearn Spring Boot Study Project first init

---
+++ advanced/src/test/kotlin/com/kakaopaysec/advanced/AdvancedApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kakaopaysec.advanced
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdvancedApplicationTests {
+
+	@Test
+	fun contextLoads() {
+		println("Spring Boot 고급 시작!")
+	}
+}
